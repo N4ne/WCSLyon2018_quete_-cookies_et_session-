bb5e7348afdc4dcc459bb5838949de288011f76a
Martin AURELLE <31614379+N4ne@users.noreply.github.com>
2018-04-10 16:44:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,11 @@ if (empty($_SESSION["login"] )) {
     header ("location: http://localhost:8000/login.php");
 }
 
+if (isset($_GET['logout']) && $_GET['logout'] == 1){
+    session_destroy();
+    header('Location: login.php');
+    die;
+}
 
 if (!empty($_GET['add_to_cart'])) {
   switch ($_GET['add_to_cart']) {
